2ae46e6f98e23ade1fcadb0347c69f3e0b27cb9c
MCwalid <benazzouzwalid82@gmail.com>
2025-04-24 11:55:02 +0100

    Premier commit de mon projet

---
+++ src/main/java/ma/enset/hopital/web/PatientController.java
@@ -0,0 +1,38 @@
+package ma.enset.hopital.web;
+
+import lombok.AllArgsConstructor;
+import ma.enset.hopital.entities.Patient;
+import ma.enset.hopital.repository.PatientRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@AllArgsConstructor
+public class PatientController {
+    private PatientRepository patientRepository;
+
+    @GetMapping("/index")
+    public String index(Model model,
+                        @RequestParam(name = "page", defaultValue = "0") int p,
+                        @RequestParam(name = "size", defaultValue = "4") int s,
+                        @RequestParam(name = "keyword", defaultValue = "") String kw) {
+        Page<Patient> pagePatients = patientRepository.findByNomContains(kw, PageRequest.of(p, s));
+        model.addAttribute("listPatients", pagePatients.getContent());
+        model.addAttribute("pages", new int[pagePatients.getTotalPages()]);
+        model.addAttribute("currentPage", p);
+        model.addAttribute("keyword", kw);
+        return "patients";
+    }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam long id,
+                         @RequestParam String keyword,
+                         @RequestParam int page) {
+        patientRepository.deleteById(id);
+        return "redirect:/index?page=" + page + "&keyword=" + keyword;
+    }
+}
